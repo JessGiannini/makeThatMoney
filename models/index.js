7c7acdfcfe2a766faab02d88d5a1bb3d8c72d4ae
JessGiannini <jessgiannini@gmail.com>
2021-07-29 19:48:05 -0700

    creating routes

--- models/index.js
+++ models/index.js
@@ -7,7 +7,6 @@ const ProductTag = require("./ProductTag");
 // Products belongsTo Category
 Product.belongsTo(Category, {
   foreignKey: "category_id",
-  targetKey: "id",
 });
 
 // Categories have many Products
@@ -17,16 +16,16 @@ Category.hasMany(Product, {
 });
 
 // Products belongToMany Tags (through ProductTag)
+//CHECK HOW TO CREATE THROUGH AS MODEL KEY
 Product.belongsToMany(Tag, {
-  through: ProductTag,
-  foreignKey: "product_id",
+  through: {
+    ProductTag,
+  },
 });
 
 // Tags belongToMany Products (through ProductTag)
 Tag.belongsToMany(Product, {
   through: ProductTag,
-  foreignKey: "tag_id",
-  onDelete: "CASCADE",
 });
 
 module.exports = {
